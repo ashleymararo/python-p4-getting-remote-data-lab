a696baeb6a60d37504f78fe347379414698d69fe
ashleymararo <ashleymararo222@gmail.com>
2026-01-13 22:46:26 +0300

    finished lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
-import requests
+
 import json
+from urllib.request import urlopen
 
 class GetRequester:
 
     def __init__(self, url):
+        if not isinstance(url,str):
+            raise TypeError("url must be a string")
         self.url = url
 
     def get_response_body(self):
-        pass
+        with urlopen(self.url) as response:
+            return response.read()
 
     def load_json(self):
-        pass
+        body=self.get_response_body()
+        return json.loads(body)
